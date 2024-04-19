ba82835f34c2e553a38aec47290b1f80ed083643
PabloTejGar <pablo.tejgar@gmail.com>
2024-04-19 11:47:03 +0200

    Initial commit.

---
+++ C#/BankKata/BankKata.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace BankKata.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ C#/BankKata/BankKata/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankKata
+{
+    public class Account
+    {
+        public int balance;
+
+        public Account(int initialBalance)
+        {
+        }
+
+        public void Deposit(int amount)
+        {
+
+        }
+
+        public void Withdraw(int amount)
+        {
+
+        }
+
+        public string PrintStatement()
+        {
+            return string.Empty;
+        }
+    }
+}
